9aed3225917707880322bc27368a0ed32d2dbfa6
dabonetn <dabone@gmail.com>
2019-08-26 19:35:53 -0400

    Update C64Key3-ProMicro.ino
    Removed German Fix, it was caused by a bug in the original keymap file.

--- C64Key3-ProMicro/C64Key3-ProMicro.ino
+++ C64Key3-ProMicro/C64Key3-ProMicro.ino
@@ -64,7 +64,6 @@
 #include <HID.h>                                    // Comment out for Teensey
 #include <Keyboard.h>                               // Comment out for Teensey
 #include "keydefs.h"                                // Comment out for Teensey
-// #include "keydefs-german.h"                      // Uncomment to fix German umlaut ä/ key 
 
 int inChar=0;
 int keyPos=0;
